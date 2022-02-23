1d2f7bf19dcbec5c8a473d69ce8fd9365979ef66
Crunck78 <crunck78@googlemail.com>
2022-02-23 10:06:40 +0100

    add signals

--- videoflix/content/signals.py
+++ videoflix/content/signals.py
@@ -1,12 +1,20 @@
+import os
 from django.dispatch import receiver
+
+from .tasks import convert_480p
 from .models import Video
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
     print('Video saved')
     if created: 
         print('New Video created')
+        convert_480p(instance.video_file.path)
 
-# post_save.connect(video_post_save, sender=Video)
+@receiver(post_delete, sender=Video)
+def video_post_delete(sender, instance, **kwargs):
+    if instance.video_file:
+        if os.path.isfile(instance.video_file.path):
+            os.remove(instance.video_file.path)
